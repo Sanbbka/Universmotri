641b13594f369733059e7ecd543c7578faac70a3
Alexander Drovnyashin <s6040780@gmail.com>
2015-11-22 15:52:13 +0300

    Initial Commit

---
+++ Universmotri/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Universmotri
+//
+//  Created by Alexander Drovnyashin on 22.11.15.
+//  Copyright © 2015 Alexander Drovnyashin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
