fbe27769acc3712adefe9da8bc7fd01cd83afce6
Alexander Drovnyashin <s6040780@gmail.com>
2016-01-03 17:14:18 +0300

    add HTMLReader framework

---
+++ Universmotri/KFUSettings.h
@@ -0,0 +1,13 @@
+//
+//  KFUSettings.h
+//  Universmotri
+//
+//  Created by Alexander Drovnyashin on 03.01.16.
+//  Copyright © 2016 Alexander Drovnyashin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KFUSettings : NSObject
+
+@end
---
+++ Universmotri/KFUSettings.m
@@ -0,0 +1,13 @@
+//
+//  KFUSettings.m
+//  Universmotri
+//
+//  Created by Alexander Drovnyashin on 03.01.16.
+//  Copyright © 2016 Alexander Drovnyashin. All rights reserved.
+//
+
+#import "KFUSettings.h"
+
+@implementation KFUSettings
+
+@end
